702b036b1f074ee925a225ffe8d6a57e4b323299
Jamie Burchfield <burchfield.camilla@gmail.com>
2023-07-13 16:37:04 -0400

    variables and reeiving input

--- File File.py
+++ File File.py
@@ -1,4 +1,28 @@
 # String / sequence of characters/ either single or double quotes 
 # Print is a function 
-print("Hello World")
-print("Jamie is testing")
+# print("Hello World")
+
+#Variables temporarily store data in a computer's memory
+#declare a variable by typing a name for variable, then equal sign, then value
+#program gets executed top to bottom, so you can update variables
+#python you can use decimal values for numbers
+#pyton you should use underscore for multiword variables 
+#can use a string as the value for a variable
+#boolean true and false
+#case sensitive language, so booleans need to be Truth or False
+# age = 33
+# age = 34
+# price = 19.95
+# first_name = "Jamie"
+# is_online = False
+# print(age)
+
+#Receiving input from the user, make sure you have the space
+# name = input("What is your name? ")
+# print("Hello " + name) #this is string concatination
+
+#Type Conversion
+#There are three types of data in Python number, string, booleans - you can convert these types
+
+birth_year = input("Enter your birth year: ")
+age = 2023 - birth_year
