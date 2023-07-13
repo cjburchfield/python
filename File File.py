e8f8fb705b691c7c1655d4dc36dedb8aeb1a050f
Jamie Burchfield <burchfield.camilla@gmail.com>
2023-07-13 16:20:53 -0400

    Hello World

---
+++ File File.py
@@ -0,0 +1,4 @@
+# String / sequence of characters/ either single or double quotes 
+# Print is a function 
+print("Hello World")
+print("Jamie is testing")
---
+++ Intro Notes.py
@@ -0,0 +1,7 @@
+Programming with Mosh - Python for Beginners
+
+Intro
+Python is the number one language for ML and Data Science projects
+Python is popular for web development when used with Django 
+Python is also useful for automating repetitive tasks
+
